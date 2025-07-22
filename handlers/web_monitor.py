0133271acf934bd460405417ae2e51f10095339e
Chingyen <42596154+bird1586@users.noreply.github.com>
2025-07-22 10:50:00 +0800

    use ngrok url

--- handlers/web_monitor.py
+++ handlers/web_monitor.py
@@ -4,13 +4,22 @@ Web monitoring functionality for Telegram bot.
 from telegram import Update
 from telegram.ext import ContextTypes
 from utils.keyboards import get_main_keyboard
+import requests
 
 # Monitor URL
-MONITOR_URL = "https://9bb6cac636e6.ngrok-free.app/"
+def get_url():
+    tunnels = requests.get('http://172.17.0.1:4040/api/tunnels').json()
+    telebot_url = "Not found"
+    for tunnel in tunnels:
+        if tunnel['name'] == 'changeDetection':
+            telebot_url = tunnel['public_url']
+    return telebot_url
+
 
 async def webmonitor_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Handle /webmonitor command."""
     reply_markup = get_main_keyboard()
+    MONITOR_URL = get_url()
     # chat_id = update.effective_chat.id
     await update.message.reply_text(
         f"🌐 **Web Monitor**\n\nMonitoring URL: {MONITOR_URL}\n\n🔗 Click the link to access the monitoring dashboard.",
@@ -21,6 +30,7 @@ async def webmonitor_command(update: Update, context: ContextTypes.DEFAULT_TYPE)
 async def handle_monit_message(update: Update) -> None:
     """Handle 'monit' message."""
     reply_markup = get_main_keyboard()
+    MONITOR_URL = get_url()
     await update.message.reply_text(
         f"🌐 **Web Monitor**\n\nMonitoring URL: {MONITOR_URL}\n\n🔗 Click the link to access the monitoring dashboard.",
         reply_markup=reply_markup,
